86117f47114ae27bba0852452d63d3c872289d7b
Danya Generalov <danya02@me.com>
2018-11-18 14:28:39 +0300

    Removing bulk decoders of images (that's the download manager's job).

--- image_coding.py
+++ image_coding.py
@@ -20,38 +20,6 @@ def write_line(surface,line,data,integer=True,rjust=True):
         if b=='1':
             surface.set_at((x,line),pygame.Color('white'))
 
-def encode(itemid,data,width,height):
-    if height<=5:
-        raise ValueError('Height must be at least 6.')
-    itemid=bin(itemid)[2:]
-    if width<len(itemid):
-        raise ValueError('Width must be at least the length of itemid in binary.')
-    itemid=itemid.rjust(width,'0')
-    bitstring = bin(int(data.hex(),16))[2:]
-    bitstring = textwrap.wrap(bitstring,width) # split by lines
-    pages=list(chunks(bitstring,height-5)) # split by pages
-    surfaces = [pygame.Surface((width,height)) for i in pages]
-    for n,i in enumerate(surfaces): # id and page total are common for all pages
-        i.fill(pygame.Color('black'))
-        write_line(i,0,itemid,False)
-        write_line(i,1,n+1)
-        write_line(i,2,len(surfaces))
-    for page,surface in zip(pages,surfaces):
-        pagelength=sum([len(q) for q in page]) # number of bits on this page
-        pagelength=bin(pagelength)[2:].rjust(2*width,'0')
-        pagelength=textwrap.wrap(pagelength,width)
-        write_line(surface,3,pagelength[0],False)
-        write_line(surface,4,pagelength[1],False)
-        for linenum,data in enumerate(page):
-            write_line(surface,linenum+5,data,False,False)
-    files=[]
-    uid=uuid.uuid4().hex
-    for n,i in enumerate(surfaces):
-        f=f'/tmp/{uid}_{n}.png'
-        pygame.image.save(i,f)
-        files.append(f)
-    return files
-
 def encode_single_page(item_id,data,width,height, part_num, max_part_num):
     if height<=5:
         raise ValueError('Height must be at least 6.')
@@ -96,27 +64,6 @@ def split_by_ids(files):
         id=int(read_line(surfaces[i],0),2)
         items.update({id:items.get(id,[])+[i]})
     return items
-
-def decode(files):
-    surfaces=[pygame.image.load(i) for i in files]
-    def page_num(surface):
-        return int(read_line(surface,1),2)
-    def last_page_num(surface):
-        return int(read_line(surface,2),2)
-    def piece_len(surface):
-        return int(read_line(surface,3)+read_line(surface,4),2)
-    if len(surfaces)!=last_page_num(surfaces[0]):
-        raise ValueError(f'Not enough pages; wait for more. (Found {len(surfaces)} out of {last_page_num(surfaces[0])})')
-    surfaces=sorted(surfaces,key=page_num)
-    total_bitstring=''
-    for i in surfaces:
-        bitstring=''
-        for n in range(5,i.get_height()):
-            bitstring+=read_line(i,n)
-        bitstring = bitstring[:piece_len(i)]
-        total_bitstring+=bitstring
-    total_bitstring=bytes.fromhex(hex(int(total_bitstring,2))[2:])
-    return total_bitstring
 
 def decode_single_page(file):
     surface = pygame.image.load(file)

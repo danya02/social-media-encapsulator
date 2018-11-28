2e53d6dd94ed4cf3f12c273cb114e3fc6a5840ca
Danya Generalov <danya02@me.com>
2018-11-28 19:34:23 +0300

    Adding a chat transmit/receive pair.

--- vk.py
+++ vk.py
@@ -36,6 +36,30 @@ class VkWallVideoTransmitter(common.Transmitter):
         upload = vk_api.VkUpload(self.connection)
         vid = upload.video(video_file=vidpath,wallpost=True)
 
+class VkChatImageTransmitter(common.Transmitter):
+    def __init__(self,connection,peer_id):
+        self.connection = connection
+        self.peer_id = peer_id
+    def send(self,id,data,part,partmax):
+        width = 2560 # make more optimal!
+        height = 2048
+        file=image_coding.encode_bw(id,data,width,height,part,partmax)
+        uploader = vk_api.upload.VkUpload(self.connection)
+        photo_id = uploader.photo_messages(file)[0]
+        api = self.connection.get_api()
+        message_id = api.messages.send(peer_id=self.peer_id,attachment=f'photo{ photo_id["owner_id"] }_{ photo_id["id"] }')
+        return json.dumps({'message':message_id,'photo':photo_id})
+    def remove(self,id):
+        id = json.loads(id)
+        api = self.connection.get_api()
+        try:
+            api.messages.delete(message_ids=str(id['message']),delete_for_all=1)
+        except:
+            pass
+        api.photos.delete(photo_id=id['photo']['id'])
+    def get_max_length():
+        return 2560*(2048-5)
+
 
 class VkWallImageReciever(common.Receiver):
     def __init__(self,connection,peer_id):
@@ -105,5 +129,36 @@ class VkWallVideoReceiver(common.Receiver):
         for i in files:
             try:
                 self.callback(*video_coding.decode_video(i))
+            except:
+                pass
+
+class VkChatImageReceiver(common.Receiver):
+    def __init__(self,connection,peer_id):
+        super().__init__()
+        self.connection = connection
+        self.peer = peer_id
+        data = self.connection.get_api().messages.getHistory(peer_id=peer_id)
+        self.last_received = data['items'][0]['id']
+    def receive_once(self):
+        api = self.connection.get_api()
+        files=[]
+        for i in api.messages.getHistory(peer_id=self.peer)['items']:
+            if i['id']>self.last_received:
+                if 'attachments' in i:
+                    for q in i['attachments']:
+                        if q['type']=='photo':
+                            maxw=0
+                            targeturl=''
+                            for n in q['photo']['sizes']:
+                                if n['width']>maxw:
+                                    maxw=n['width']
+                                    targeturl=n['url']
+                            files += [f'/tmp/{uuid.uuid4().hex}.jpg']
+                            with requests.get(targeturl) as r:
+                                with open(files[-1],'wb') as o:
+                                    o.write(r.content)
+        for i in files:
+            try:
+                self.callback(*image_coding.decode_bw(i))
             except:
                 pass
